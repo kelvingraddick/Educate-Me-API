7ee619b9a7af2e07114dc0bd31c843b760345937
Kelvin Graddick <kelvingraddick@kg.codes>
2021-01-29 07:44:55 -0500

    Educators list endpoint

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var educatorRouter = require('./routes/educator');
+var educatorsRouter = require('./routes/educators');
 var employerRouter = require('./routes/employer');
 
 var app = express();
@@ -25,6 +26,7 @@ app.use(cors());
 app.use('/', indexRouter);
 app.use('/educator', educatorRouter);
 app.use('/employer', employerRouter);
+app.use('/educators', educatorsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/educators.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var Database = require('../helpers/database');
+
+router.get('/', async function(req, res, next) {
+  var response = { isSuccess: false };
+
+  var query = Database.Educator.find();
+
+  if (req.query.query) {
+    query.or([
+      { 'name.first': { $regex: req.query.query || '', $options: 'i' } },
+      { 'name.last': { $regex: req.query.query || '', $options: 'i' } }
+    ])
+  }
+
+  if (req.query.location) {
+    query.where('locations', { $regex: req.query.location || '', $options: 'i' });
+  }
+
+  query.select('-password');
+  query.setOptions({
+    skip: parseInt(req.query.skip),
+    limit: parseInt(req.query.limit)
+  });
+
+  response.educators = await query
+    .exec()
+    .catch((error) => { response.errorMessage = error.message; });
+
+  response.isSuccess = response.educators != null;
+  
+  res.json(response);
+});
+
+module.exports = router;
