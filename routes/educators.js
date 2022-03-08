136800b9f72f736ed7528612c247d13c393ad61d
Kelvin Graddick <kelvingraddick@kg.codes>
2022-03-08 00:29:46 -0500

    Educators matches endpoint

--- routes/educators.js
+++ routes/educators.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var authorize = require('../middleware/authorize');
 var Database = require('../helpers/database');
 
 router.get('/', async function(req, res, next) {
@@ -35,6 +36,36 @@ router.get('/', async function(req, res, next) {
   response.isSuccess = response.educators != null;
   
   res.json(response);
+});
+
+router.get('/matches', authorize, async function(req, res, next) {
+    var response = {
+      isSuccess: false,
+      matches: []
+    };
+
+    const allEducators  = await Database.Educator
+      .find()
+      .select('-password')
+      .exec()
+      .catch((error) => { response.errorMessage = error.message; });
+
+    const allJobs = await Database.Job
+      .find()
+      .exec()
+      .catch((error) => { response.errorMessage = error.message; });
+
+    for (const educator of allEducators) {
+      var match = {
+        educator: educator,
+        jobs: allJobs.filter(x => educator.locations && educator.locations.includes((x.city + ', ' + x.state)))
+      };
+      response.matches.push(match);
+    }
+    
+    response.isSuccess = response.matches.length > 0;
+    
+    res.json(response);
 });
 
 module.exports = router;
