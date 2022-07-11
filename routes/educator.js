612c2fd0721ca7529c5808b5d00871a945f3f320
Kelvin Graddick <kelvingraddick@kg.codes>
2022-07-11 02:43:32 -0400

    Null checks for isAdmin flags

--- routes/educator.js
+++ routes/educator.js
@@ -54,7 +54,7 @@ router.post('/:educatorId/update', authorize, async function(req, res, next) {
   var existingEducator = await Database.Educator.findOne({ emailAddress: req.body.emailAddress }).exec();
   if (existingEducator && existingEducator._id != educatorId) {
     res.json({ isSuccess: false, errorCode: ErrorType.EMAIL_TAKEN, errorMessage: 'This email address is already taken.' });
-  } else if (educatorId != req.educator.id && !req.educator.isAdmin && !req.employer.isAdmin) {
+  } else if (educatorId != req.educator?.id && !req.educator?.isAdmin && !req.employer?.isAdmin) {
     res.sendStatus(403);
   } else {
     var updatedEducator = {
@@ -95,7 +95,7 @@ router.post('/:educatorId/update', authorize, async function(req, res, next) {
 
 router.delete('/:educatorId/delete', authorize, async function(req, res, next) {
   var educatorId = req.params.educatorId;
-  if (educatorId != req.educator.id && !req.educator.isAdmin && !req.employer.isAdmin) {
+  if (educatorId != req.educator?.id && !req.educator?.isAdmin && !req.employer?.isAdmin) {
     res.sendStatus(403);
   } else {
     Database.Educator.deleteOne({ _id: educatorId })
@@ -121,7 +121,7 @@ router.get('/:id', async function(req, res, next) {
 
 router.get('/:educatorId/jobs', authorize, async function(req, res, next) {
   var educatorId = req.params.educatorId;
-  if (educatorId != req.educator.id && !req.educator.isAdmin && !req.employer.isAdmin) {
+  if (educatorId != req.educator?.id && !req.educator?.isAdmin && !req.employer?.isAdmin) {
     res.sendStatus(403);
   } else {
     var response = { isSuccess: false };
